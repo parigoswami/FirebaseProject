03c9f837aecbd7d69b1eab1f6505e919c1d99f36
parigoswami <goswamiutangpari@yahoo.in>
2017-08-07 14:01:59 +0530

    Update myTableViewController.swift

--- FirebaseProject/myTableViewController.swift
+++ FirebaseProject/myTableViewController.swift
@@ -65,14 +65,12 @@ class myTableViewController: UITableViewController {
         return cell
     }
  
-
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
     
     // Override to support editing the table view.
